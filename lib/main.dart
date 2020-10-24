6fb5db4435cb0a67c4a33f2fdd6d6b7f70a896a2
June Ligan <june.ligan@synacy.com>
2020-10-24 12:04:09 +0800

    Push new changes with calendar display 	new screen for adding a new event
    Signed-off-by: June Ligan <june.ligan@synacy.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,19 @@
+import 'package:calendar_event/add_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primarySwatch: Colors.blueGrey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,84 +24,225 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  CalendarController _calendarController;
+  Map<DateTime, List> _events = Map<DateTime, List>();
+  List _selectedEvents = [];
+  DateTime _selectedDay = DateTime.now();
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+
+    _events = {
+      _selectedDay.subtract(Duration(days: 30)): [
+        'Event A0',
+        'Event B0',
+        'Event C0'
+      ],
+      _selectedDay.subtract(Duration(days: 27, hours: 5)): ['Event A1'],
+      _selectedDay: [
+        'Event A2',
+        'Event B2',
+        'Event C2',
+        'Event D2',
+      ]
+    };
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedEvents = _events[_selectedDay];
     });
   }
 
   @override
+  void dispose() {
+    _calendarController.dispose();
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          TableCalendar(
+            events: _events,
+            calendarController: _calendarController,
+            onDaySelected: _onDaySelected,
+            builders: CalendarBuilders(
+              markersBuilder: (context, date, events, holidays) {
+                final children = <Widget>[];
+
+                if (events.isNotEmpty) {
+                  children.add(
+                    Positioned(
+                      right: 1,
+                      bottom: 1,
+                      child: _buildEventsMarker(date, events),
+                    ),
+                  );
+                }
+                return children;
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+          Expanded(child: _buildEventList()),
+        ],
       ),
+
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          _navigateToAddEvent(context);
+        },
+        tooltip: 'Add Event',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+  
+  static _navigateToAddEvent(BuildContext context) async {
+    var result = await Navigator.push(context, new MaterialPageRoute(
+      builder: (BuildContext ctx) => AddScreen(),
+      fullscreenDialog: true,)
+    );
+
+    Scaffold.of(context).showSnackBar(SnackBar(content: Text("$result"),duration: Duration(seconds: 3),));
+  }
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.red[800]
+      ),
+      width: 18.0,
+      height: 18.0,
+      child: Center(
+        child: Text(
+          '${events.length}',
+          style: TextStyle().copyWith(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onDaySelected(DateTime day, List events, List holidays) {
+    print('CALLBACK: _onDaySelected');
+    print(events);
+    setState(() {
+      _selectedEvents = events;
+    });
+  }
+
+  Widget _buildEventList() {
+    List<dynamic> events = _selectedEvents ?? [];
+    return ListView(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      children: events
+          .map((event) => Container(
+        decoration: BoxDecoration(
+          color: Colors.red[100],
+          border: Border(bottom: BorderSide(
+              color: Colors.blueAccent,
+              width: 0.4
+          )),
+//          borderRadius: BorderRadius.circular(12.0),
+        ),
+//        margin:
+//        const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: ListTile(
+          title: Text(event.toString()),
+          onTap: () => print('$event tapped!'),
+        ),
+      ))
+          .toList(),
+    );
+  }
+
+  static Future<String> createAlertDialog(BuildContext context, String dialogText, String buttonText) {
+    TextEditingController eventNameController = TextEditingController();
+    TextEditingController dateNameController = TextEditingController();
+
+    return showDialog(context: context, builder: (context) {
+      return AlertDialog(
+          title: Text(dialogText),
+          content:Stack(
+              overflow: Overflow.visible,
+              children: <Widget>[
+                TextField(
+                    textCapitalization: TextCapitalization.sentences,
+                    controller: eventNameController
+                ),
+                TextField(
+                    textCapitalization: TextCapitalization.sentences,
+                    controller: dateNameController
+                ),
+              ]
+          ),
+          actions: <Widget>[
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context).pop(eventNameController.text.toString());
+              },
+              elevation: 5.0,
+              child: Text(buttonText),
+            )
+          ]
+      );
+    });
+  }
+
+  showEventDialog(BuildContext context, {
+        TextEditingController usernameController,
+        TextEditingController loginController,
+        Function onPressed, String title, List<Widget> widgets,
+        String actionName }) {
+    Alert(
+        context: context,
+        title: title,
+        content: Column(
+          children: widgets
+        ),
+        buttons: [
+          DialogButton(
+            onPressed: onPressed,
+            child: Text(
+              actionName,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          )
+        ]).show();
+  }
+}
+
+class CalendarEvent {
+  String name;
+  DateTime date;
+  Duration duration;
+  Color eventColor;
+
+  CalendarEvent(name, date, duration, eventColor) {
+    this.name = name;
+    this.date = date;
+    this.duration = duration;
+    this.eventColor = eventColor;
+  }
+
+  CalendarEvent build(name, date, duration, eventColor) {
+    return CalendarEvent(name, date, duration, eventColor);
   }
 }
